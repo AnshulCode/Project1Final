89c98580603e85dde26c91811e9da1a27ac08942
steff0508 <62529746+steff0508@users.noreply.github.com>
2021-03-03 21:39:34 -0500

    Add files via upload

--- getfile.c
+++ getfile.c
@@ -5,10 +5,16 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <regex.h>
 struct stat sb;
 
 void listFiles(const char *path)
 {
+	const char *pattern = "^wrap\\..*";
+	regex_t regex;
+	int rc;
+
+
 	struct dirent *dp;
 	DIR *dir = opendir(path);
 
@@ -18,7 +24,10 @@ void listFiles(const char *path)
 	while ((dp = readdir(dir)) != NULL)
 	{
 		stat(dp->d_name, &sb);
-		if (S_ISREG(sb.st_mode))
+		rc = regcomp(&regex, pattern, 0);
+		rc = regexec(&regex, dp->d_name, 0, NULL, 0);
+		//printf("name: %s, rc = %d\n", dp->d_name, rc);
+		if (S_ISREG(sb.st_mode) && dp->d_name[0] != '.' && rc)
 			printf("%s\n", dp->d_name);
 	}
 
@@ -33,7 +42,7 @@ int main(int argc, char* argv[]) {
 		if (S_ISDIR(sb.st_mode))
 			listFiles(argv[1]);
 	}
-	printf("\n");
+	//printf("\n");
 	return 0;
 }
 
