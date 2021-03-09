78d3b96482ca015a4ab0b3b123fa6b924a63b164
steff0508 <62529746+steff0508@users.noreply.github.com>
2021-03-08 22:32:09 -0500

    Add files via upload

--- ww.c
+++ ww.c
@@ -1,3 +1,4 @@
+
 #include <fcntl.h> 
 #include <unistd.h>
 #include <stdio.h>
@@ -7,124 +8,137 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <regex.h>
-
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
-#ifndef BFSIZE
-#define BFSIZE 256
-#endif
+#include <ctype.h>
+#define BUFF_LENGTH (256)
+#define BYTES_READ (256)
 struct stat sb;
+char buff[BUFF_LENGTH];
 
-char buff[BFSIZE];
 void get_file(const char *path)
 {
-        const char *pattern = "^wrap\\..*";
-        regex_t regex;
-        int rc;
+	const char *pattern = "^wrap\\..*";
+	regex_t regex;
+	int rc;
+	struct dirent *dp;
+	DIR *dir = opendir(path);
 
+	if (!dir)
+		return;
 
-        struct dirent *dp;
-        DIR *dir = opendir(path);
+	while ((dp = readdir(dir)) != NULL)
+	{
+		stat(dp->d_name, &sb);
+		if (S_ISREG(sb.st_mode))
+			printf("%s\n", dp->d_name);
+	}
 
-        if (!dir)
-                return;
-
-        while ((dp = readdir(dir)) != NULL)
-        {
-                stat(dp->d_name, &sb);
-                rc = regcomp(&regex, pattern, 0);
-                rc = regexec(&regex, dp->d_name, 0, NULL, 0);
-                //printf("name: %s, rc = %d\n", dp->d_name, rc);
-                if (S_ISREG(sb.st_mode) && dp->d_name[0] != '.' && rc)
-                        printf("%s\n", dp->d_name);
-        }
-
-        closedir(dir);
+	closedir(dir);
 }
 
-int word_wrap(int limit, char* name){
-	int fd;
-	if (name){
-		char* fname = name;
-		fd = open(fname,O_RDONLY);
-	} else {
-		fd = 0;
-		printf("press enter when complete your input:\n");
-	}
+int word_wrap(int limit, char* filename){
+
+	char* fname = filename;
 	int lim = limit;
-	int bytes_read = read(fd,buff,BFSIZE);
-	printf("bytes_read: %d", bytes_read);
-	putchar('\n');
-	printf("%s",buff);
+	int fd;
+	int size = BYTES_READ;
+	int bytes_read;
+	if(fname){
+		fd = open(fname,O_RDONLY);
+		bytes_read = read(fd,buff,size);
+	}else{
+		fd = open(0,O_RDONLY); 
+		bytes_read = read(0,buff,size);
+	}
+
+	int str = strlen(buff); 
+	if(bytes_read ==  str && fd !=0){
+		while(bytes_read ==  str){
+			close(fd);
+			size = size*2;
+			fd = open(fname,O_RDONLY);
+			read(fd,buff,size);
+			bytes_read = read(fd,buff,size);
+			str = strlen(buff); 
+		}
+	}
+
 	int i = 0;
 	int count_tot = 0;
 	int count_word = 0;
-	int newline = 0;
-	putchar('\n');
-	printf("strlen : %lu",strlen(buff) );
-	putchar('\n');
+	int sizer = 0;
 
 	strbuf_t* word = (strbuf_t*)malloc(sizeof(strbuf_t));
 	sb_init(word,1);
+	strbuf_t* new_buf = (strbuf_t*)malloc(sizeof(strbuf_t));
+	sb_init(new_buf,size);
+
 	// prints word wrapping needs work but base case works
 	while(buff[i]!= '\0' && i<strlen(buff)-1){
-		/*
-		   if(buff[i] == ' '){
-		   while(buff[i] == ' ' || buff[i] == '\n'){
-		   i++;
-		   }
-		   }
-		   */
-		while(buff[i] == ' ') {
-			//if(DEBUG) printf("i:%d,", i);
-			i++;
+		if(isspace(buff[i])!=0&& buff[i]!='\n'){
+			while(isspace(buff[i])!=0){
+				i++;
+			}
 		}
-		if(buff[i] == '\n') { 
-			if(DEBUG) printf("nl:%d ", newline);
-			if (DEBUG) printf("(%d,%d)", count_word, count_tot);
-			newline = 1;
-		}
-
-		while(buff[i] != ' '){
-			if(buff[i] == '\n' || buff[i] == '\0'){
+		while(isspace(buff[i])==0){
+			if(buff[i] == '\n' ||buff[i] == '\0'){
 				break;
 			}
+			sizer++;
 			sb_append(word,buff[i]);
 			count_word++;
-			//printf(" >>count_word: %d, count_tot: %d\n", count_word, count_tot);
 			i++;
+		}
+		if(buff[i] == '\n' && count_word == 0){
+			int count = 0;
+			//sb_append(new_buf,'\n');
+			while(isspace(buff[i])!= 0){
+				if(buff[i] == '\n'){
+					count++;
+					if(count == 2){
+						sb_append(new_buf,'\n');
+						sb_append(new_buf,'\n');
+						sizer = sizer+2;
+					}else if(count >2){
+						sb_append(new_buf,'\n');
+					}
+				}
+				i++;
+			}
+			//`AQprint_string(word);
+			word = (strbuf_t*)malloc(sizeof(strbuf_t));
+			sb_init(word,1);
+			count_tot = 0;
 		}
 
 		if(count_word>=lim){
-			exit(EXIT_FAILURE);
+			return EXIT_FAILURE;
 		}
+		if(count_word+count_tot <lim && count_word != 0){
 
-		if (DEBUG) printf("(%d,%d)", count_word, count_tot);
-		if (newline) {
-			if(DEBUG) printf("<nl>");
-			putchar('\n');
-			putchar('\n');
-			newline = 0;
-			count_tot = 0;
-			count_word = 0;
-		}
-		if(count_word+count_tot <lim){
-			//if (DEBUG) printf("if:(%d,%d)", count_word, count_tot);
-			print_string(word);
-			putchar(' ');
+			int ind = 0;
+			while(word->data[ind]!='\0'){
+				sb_append(new_buf,word->data[ind]);
+				ind++;
+			}
+			sb_append(new_buf,' ');
+			sizer++;
 			word = (strbuf_t*)malloc(sizeof(strbuf_t));
 			sb_init(word,1);
 			i++;
 			count_tot = count_tot+count_word+1;
 			//count_tot++;
 			count_word = 0;
-		}else{
-			//if (DEBUG) printf("else:(%d,%d)", count_word, count_tot);
-			putchar('\n');
-			print_string(word);
-			putchar(' ');
+		}else if(count_word != 0){
+			sb_append(new_buf,'\n');
+
+			sizer++;
+			int ind = 0;
+			while(word->data[ind]!='\0'){
+				sb_append(new_buf,word->data[ind]);
+				ind++;
+			}
+			sb_append(new_buf,' ');
+			sizer++;
 			word = (strbuf_t*)malloc(sizeof(strbuf_t));
 			sb_init(word,1);
 			i++;
@@ -133,8 +147,23 @@ int word_wrap(int limit, char* name){
 		}
 
 	}
+
+	sb_append(new_buf,'\n');
+	sizer++;
+
+	char* write_data = (char*)malloc(sizeof(char)*sizer);
+	int fin = 0;
+	while(fin<sizer){
+		write_data[fin] = new_buf->data[fin];
+		fin++;
+	}
+
+	write_data = (char*)realloc(write_data,sizeof(char)*(new_buf->used));
+	free(new_buf);
+	write(1,write_data,sizer);
 	close(fd);
-	return 0;
+
+	return EXIT_SUCCESS;
 }
 
 int main(int argc, char* argv[]) {
@@ -147,8 +176,10 @@ int main(int argc, char* argv[]) {
 		printf("ERROR: line width must be positive\n");
 		exit(EXIT_FAILURE);
 	}
-	if (argc == 2)
+	if (argc == 2){
+		printf("-- Press 'enter' when you complete your input -- \n");
 		word_wrap(width, NULL);
+	}
 	if (argc == 3){
 		stat(argv[2], &sb);
 		if (S_ISREG(sb.st_mode))
@@ -159,7 +190,4 @@ int main(int argc, char* argv[]) {
 	printf("\n");
 	return 0;
 }
-
-
-
 
